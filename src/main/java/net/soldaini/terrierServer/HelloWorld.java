a99236a1fae3b6637b935a891d05a99b9e6acc22
Luca Soldaini <luca@soldaini.net>
2017-05-29 18:13:29 -0400

    removed sample code

--- src/main/java/net/soldaini/terrierServer/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package net.soldaini.terrierServer;
-import static spark.Spark.*;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
-}
